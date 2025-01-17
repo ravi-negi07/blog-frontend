a1b74ebe71247fe908db87579d1f0223427b400d
ravi <negiravi451@gmail.com>
2025-01-17 14:33:00 +0530

    Set up the application entry point in index.js and render App component

--- src/utills/index.ts
+++ src/utills/index.ts
@@ -1,6 +1,7 @@
 import React from "react";
+const Home = React.lazy(() => import("../pages/Home"));
 const SignUp = React.lazy(() => import("../pages/SignUp"));
 const Login = React.lazy(() => import("../pages/Login"));
 const Header = React.lazy(() => import("../Layouts/Header"));
 const About = React.lazy(() => import("../pages/About"));
-export { SignUp, Login, Header, About };
+export { SignUp, Login, Header, About, Home };
